cfa22ad8f0035f341821b92b752e61099cf2736a
Lorenzo Caselli <lore.caselli97@gmail.com>
2026-01-31 02:18:13 +0100

    updated signals for dialogue system for functionalities

---
+++ crpg_dialogue_demo/DialogueBox.gd
@@ -0,0 +1,173 @@
+extends VBoxContainer
+
+@export var dialogue: JSON
+
+@onready var dialogue_choice_res = preload("res://crpg_dialogue_demo/DialogueButton.tscn")
+
+@export var state: Dictionary = {}
+var dialogue_finished = false
+var is_rolling = false
+
+var button_cache: Array[DialogueButton] = []
+
+@onready var dialogue_handler: EzDialogue = $EzDialogue
+#@onready var roll_handler = $"../RollBox"
+@onready var sprites_handler = $SpriteHandler
+
+func _ready():
+	dialogue_finished = false
+	dialogue_handler.start_dialogue(dialogue, state)
+
+func clear_dialogue():
+	$text.text = ""
+	is_rolling = false
+	for child in get_children():
+		if child is Button:
+			child.hide()
+
+func add_text(text: String):
+	$text.text = text
+
+func add_choice(choice_text: String, id: int):
+	if button_cache.size() < id + 1:
+		var new_button = dialogue_choice_res.instantiate()
+		new_button.choice_id = id
+		button_cache.push_back(new_button)
+		add_child(new_button)
+		new_button.dialogue_selected.connect(_on_choice_button_down)
+
+	var button = button_cache[id]
+	button.text = choice_text
+	button.show()
+
+func _on_choice_button_down(choice_id: int):
+	clear_dialogue()
+	if !dialogue_finished:
+		dialogue_handler.next(choice_id)
+
+func _on_ez_dialogue_dialogue_generated(response: DialogueResponse):
+	if is_rolling:
+		return
+
+	add_text(response.text)
+	if response.choices.is_empty():
+		add_choice("[...]", 0)
+	else:
+		for i in response.choices.size():
+			add_choice(response.choices[i], i)
+
+func _on_ez_dialogue_end_of_dialogue_reached():
+	dialogue_finished = true
+
+func _on_ez_dialogue_custom_signal_received(value: String):
+	var params = value.split(",")
+
+	########################### DIALOGUE PARAMETERS SIGNALS HANDLED IN THIS SECTION ###########################
+	if params[0] == "changeparam":
+		var param_name: String = params[1]
+		var amount: int
+		if params.size() < 3 or not params[2].is_valid_int():
+			print("[changeparam] Warning: Invalid amount parameter.")
+			return
+		amount = int(params[2])
+		if param_name in state:
+			state[param_name] += amount
+			print(param_name + "': " + str(state[param_name]))
+		else:
+			print("[changeparam] Warning: Parameter '" + param_name + "' not found in state dictionary.")
+	elif params[0] == "setparam":
+		var param_name: String = params[1]
+		var amount: int
+		if params.size() < 3 or not params[2].is_valid_int():
+			print("[setparam] Warning: Invalid amount parameter.")
+			return
+		amount = int(params[2])
+		if param_name in state:
+			state[param_name] = amount
+			print("Set parameter '" + param_name + "' to: " + str(state[param_name]))
+		else:
+			print("[setparam] Warning: Parameter '" + param_name + "' not found in state dictionary.")
+	elif params[0] == "debug":
+		var message: String = params[1]
+		print("[print signal]: " + message)
+	elif params[0] == "checkparam":
+		var comparison_type: String = params[1]
+		var param_name: String = params[2]
+		var test_value: int
+
+		if params.size() < 4 or not params[3].is_valid_int():
+			print("[checkparam] Warning: Invalid test value parameter.")
+			return
+		test_value = int(params[3])
+
+		if param_name in state:
+			var current_value = state[param_name]
+			var comparison_result = false
+
+			match comparison_type:
+				"<":
+					comparison_result = current_value < test_value
+				">":
+					comparison_result = current_value > test_value
+				"<=":
+					comparison_result = current_value <= test_value
+				">=":
+					comparison_result = current_value >= test_value
+				"==":
+					comparison_result = current_value == test_value
+				"!=":
+					comparison_result = current_value != test_value
+				_:
+					print("[checkparam] Warning: Unrecognized comparison type '" + comparison_type + "'.")
+					return
+
+			print("[checkparam] Comparison result for '" + param_name + "': " + str(comparison_result))
+			state["comparison_result"] = comparison_result
+		else:
+			print("[checkparam] Warning: Parameter '" + param_name + "' not found in state dictionary.")
+	elif params[0] == "highestparam":
+		var highest_param: String = ""
+		var highest_value: int = -INF
+
+		for key in state.keys():
+			if state[key] is int:
+				var param_value: int = state[key]
+				if param_value > highest_value:
+					highest_value = param_value
+					highest_param = key
+				elif param_value == highest_value:
+					highest_param = ""  # Tie detected
+					print("[highestparam] Warning: Tie detected between multiple parameters with value: " + str(highest_value))
+					state["highest_param"] = highest_param
+					return
+
+		if highest_param != "":
+			print("[highestparam] Highest parameter is '" + highest_param + "' with value: " + str(highest_value))
+			state["highest_param"] = highest_param
+		else:
+			print("[highestparam] Warning: Either No integer parameters found in state dictionary or no states in it at all.")
+
+	########################### VISUAL PARAMETERS SIGNALS HANDLED IN THIS SECTION ###########################
+	elif params[0] == "changesprites":
+			var left_character_name: String = params[1]
+			var left_character_expression: String = params[2]
+			var right_character_name: String = params[3]
+			var right_character_expression: String = params[4]
+			var left_character: String = left_character_name + "_" + left_character_expression
+			var right_character: String = right_character_name + "_" + right_character_expression
+			
+			sprites_handler.change_characters_visual(left_character, right_character)
+
+	########################### UNHANDLED SIGNALS HANDLED IN THIS SECTION ###########################
+	else:
+		print("[custom_signal] Error: There are no parameters/unrecognized ones in the specified signals. Following are the available signals:")
+		print("PARAMETERS SIGNALS:")
+		print("signal(changeparam,\"paramname\",value)")
+		print("signal(setparam,\"paramname\",value)")
+		print("signal(debug,\"message\")")
+		print("signal(checkparam,\"<|>|<=|>=|==|!=\",\"paramname\",value) => comparison_result")
+		print("signal(highestparam) => highest_param")
+		print("VISUAL SIGNALS:")
+		print("signal(changesprites,\"leftcharactername\",\"leftcharacterexpression\",\"rightcharactername\",\"rightcharacterexpression\")")
+		
+		
